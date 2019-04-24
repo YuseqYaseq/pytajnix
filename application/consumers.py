c7ea6bb68af198c1a7e4a416193aa0be8d76f8e3
YuseqYaseq <jozefjasek@gmail.com>
2019-04-24 20:22:07 +0200

    Replaced sockets with asynchronous version.

--- application/consumers.py
+++ application/consumers.py
@@ -1,50 +1,45 @@
 # application/consumers.py
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 from asgiref.sync import async_to_sync
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
-        print(self.scope['url_route']['kwargs'])
-        self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = 'chat_%s' % self.room_name
+class LecturerConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.lecture_name = self.scope['url_route']['kwargs']['lecture_name']
+        self.lecture_group_name = 'chat_%s' % self.lecture_name
 
         # Join room group
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name,
+        await self.channel_layer.group_add(
+            self.lecture_group_name,
             self.channel_name
         )
-        print("connect")
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, close_code):
+    async def disconnect(self, close_code):
         # Leave room group
-        async_to_sync(self.channel_layer.group_discard)(
-            self.room_group_name,
+        await self.channel_layer.group_discard(
+            self.lecture_group_name,
             self.channel_name
         )
-        print("disconnect")
 
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
         # Send message to room group
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name,
+        await self.channel_layer.group_send(
+            self.lecture_group_name,
             {
                 'type': 'chat_message',
                 'message': message
             }
         )
-        print("receive")
 
-        # Receive message from room group
-    def chat_message(self, event):
+    # Receive message from room group
+    async def chat_message(self, event):
         message = event['message']
 
         # Send message to WebSocket
-        self.send(text_data=json.dumps({
+        await self.send(text_data=json.dumps({
             'message': message
         }))
-        print("chat_message")
--- application/routing.py
+++ application/routing.py
@@ -4,5 +4,5 @@ from django.conf.urls import url
 from . import consumers
 
 websocket_urlpatterns = [
-    url(r'^ws/application/(?P<room_name>[^/]+)/$', consumers.ChatConsumer),
+    url(r'^ws/application/(?P<lecture_name>[^/]+)/$', consumers.LecturerConsumer),
 ]
