3ee2e95283a7ba3c2369ef897761ef4523815e3b
YuseqYaseq <jozefjasek@gmail.com>
2019-05-15 20:02:10 +0200

    Added lecturer channel consumer.

---
+++ application/consumers_def/common.py
@@ -0,0 +1,17 @@
+from channels.generic.websocket import AsyncWebsocketConsumer
+import json
+
+from application.models import Question
+
+# group types
+private_msg_group_name = 'private_msg_'
+question_group_name = 'question_'
+approved_question_group_name = 'approved_question_'
+
+# msg types:
+m_private = "msg_private"
+m_question = "msg_question"
+m_edit = "msg_edit"
+m_delete = "msg_delete"
+m_approve = "msg_approve"
+m_vote = "msg_vote"
